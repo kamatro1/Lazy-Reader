2e262e7f76e55226313d7204b69c2316ed482f58
emmrlee <emmaruthlee@gmail.com>
2024-04-10 23:35:41 -0400

    Style: create skeleton divs; colors for debugging

--- src/sidepanel/index.tsx
+++ src/sidepanel/index.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SidePanel from './SidePanel';
 
-const root = document.createElement("div");
-root.className = "container";
-document.body.appendChild(root);
-const rootDiv = ReactDOM.createRoot(root);
-rootDiv.render(
+const header = document.createElement("div");
+header.className = "header";
+document.body.appendChild(header);
+
+const headerRoot = ReactDOM.createRoot(header);
+headerRoot.render(
+  <React.StrictMode>
+    <SidePanel />
+  </React.StrictMode>
+);
+
+const content = document.createElement("div");
+content.className = "content";
+document.body.appendChild(content);
+
+const contentRoot = ReactDOM.createRoot(content);
+contentRoot.render(
   <React.StrictMode>
     <SidePanel />
   </React.StrictMode>
