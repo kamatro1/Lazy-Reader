3282e1a7601e7d08c02623ba607e71655a0530b4
emmrlee <emmaruthlee@gmail.com>
2024-04-17 18:57:28 -0400

    Style: add hover & on click toggle style logic

--- src/sidepanel/Header.tsx
+++ src/sidepanel/Header.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SidePanel.css';
 
 
 function Header() {
+  const [bulletPoint, setBulletPoints] = useState<boolean>(true);
+  const [checkbox, setCheckbox] = useState<boolean>(false);
+  
   const handleClick = () => {
     chrome.runtime.openOptionsPage();
   }
@@ -15,13 +18,13 @@ function Header() {
             <h1 id='title' className='heading-text'>LazyReader</h1>
           </div>
           <div id='header-top-buttons' className='header-top-buttons'>
-            <img id='settings32' className='button' src='../icons/playful-ui/settings/settings32.svg' onClick={handleClick}></img>
+            <img id='settings32' className='button' src='../icons/playful-ui/settings/settings32.svg' onClick={handleClick} onMouseOver={e => (e.currentTarget.src = '../icons/playful-ui/settings/settings32-filled.svg')} onMouseOut={e => (e.currentTarget.src = '../icons/playful-ui/settings/settings32.svg')}></img>
           </div>
         </div>
         <div id='header-bottom' className='header-bottom'>
           <div id='header-bottom-options' className='header-bottom-options'>
-            <img id='bullet' className='button' src='../icons/playful-ui/bullet/bullet32-filled.svg'></img>
-            <img id='paragraph' className='button' src='../icons/playful-ui/paragraph/paragraph32.svg'></img>
+            {bulletPoint ? <img id='bullet' className='button' src='../icons/playful-ui/bullet/bullet32-filled.svg'></img> : <img id='bullet' className='button' src='../icons/playful-ui/bullet/bullet32.svg' onClick={() => setBulletPoints(!bulletPoint)} onMouseOver={e => (e.currentTarget.src = '../icons/playful-ui/bullet/bullet32-filled.svg')} onMouseOut={e => (e.currentTarget.src = '../icons/playful-ui/bullet/bullet32.svg')}></img>}
+            {!bulletPoint ? <img id='paragraph' className='button' src='../icons/playful-ui/paragraph/paragraph32-filled.svg'></img> : <img id='paragraph' className='button' src='../icons/playful-ui/paragraph/paragraph32.svg' onClick={() => setBulletPoints(!bulletPoint)} onMouseOver={e => (e.currentTarget.src = '../icons/playful-ui/paragraph/paragraph32-filled.svg')} onMouseOut={e => (e.currentTarget.src = '../icons/playful-ui/paragraph/paragraph32.svg')}></img>}
             <div id='reading-level-container' className='reading-level-container'>
               <h1 id='reading-level-text' className='dropdown-text'>Lvl:</h1>
               <img id='reading-level-dropdown' className='reading-level-dropdown' src='../icons/playful-ui/dropdown/reading-level-dropdown.svg'></img>
@@ -31,7 +34,7 @@ function Header() {
               <img id='length-dropdown' className='length-dropdown' src='../icons/playful-ui/dropdown/length-dropdown.svg'></img>
             </div>
           </div>
-          <img id='checkbox32' className='button' src='../icons/playful-ui/checkbox/checkbox32-checked-filled.svg'></img>
+          {checkbox ? <img id='checkbox32' className='button' src='../icons/playful-ui/checkbox/checkbox32-checked-filled.svg'></img> : <img id='checkbox32' className='button' src='../icons/playful-ui/checkbox/checkbox32-checked.svg' onClick={() => setCheckbox(!checkbox)} onMouseOver={e => (e.currentTarget.src = '../icons/playful-ui/checkbox/checkbox32-checked-filled.svg')} onMouseOut={e => (e.currentTarget.src = '../icons/playful-ui/checkbox/checkbox32-checked.svg')}></img>}
         </div>
       </div>
   );
