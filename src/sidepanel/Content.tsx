2388b670fe6f02b9916c570a45058626c05ff6a8
emmrlee <emmaruthlee@gmail.com>
2024-04-20 21:04:34 -0400

    Feat: timeout copy icon to uncheck after 3 seconds

--- src/sidepanel/Content.tsx
+++ src/sidepanel/Content.tsx
@@ -8,7 +8,15 @@ function Content() {
     <div id='content' className='content'>
       <div id='summary-header' className='summary-header'>
         <h1 id='summary-heading' className='heading-text'>Summary:</h1>
-        {copy ? <img id='copy32' className='button' src='../icons/playful-ui/copy/copy32-checked.svg'></img> : <img id='copy32' className='button' src='../icons/playful-ui/copy/copy32.svg' onClick={() => setCopy(!copy)} onMouseOver={e => (e.currentTarget.src = '../icons/playful-ui/copy/copy32-filled.svg')} onMouseOut={e => (e.currentTarget.src = '../icons/playful-ui/copy/copy32.svg')}></img>}
+        {copy ? <img id='copy32' className='button' src='../icons/playful-ui/copy/copy32-checked.svg'></img> : <img id='copy32' className='button' src='../icons/playful-ui/copy/copy32.svg' 
+          onClick={() => {
+            setCopy(true);
+            setTimeout(() => {
+              setCopy(false);
+            }, 3000);
+          }} 
+          onMouseOver={e => (e.currentTarget.src = '../icons/playful-ui/copy/copy32-filled.svg')} 
+          onMouseOut={e => (e.currentTarget.src = '../icons/playful-ui/copy/copy32.svg')}></img>}
       </div>
       <div id ='summary-text-box' className='summary-text-box'>
       </div>
