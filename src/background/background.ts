76e31afbab9c7c3489813677ddfc179ee3fdbbb5
annieyfwang <anniewangre@gmail.com>
2024-04-23 13:11:19 -0400

    Update listener for scraping

--- src/background/background.ts
+++ src/background/background.ts
@@ -4,5 +4,20 @@ chrome.sidePanel
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log("I just installed my chrome extension")
-})
+}); 
+
+function sendMessageToContentScript() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (tabs.length > 0) {
+      const activeTabId = tabs[0].id;
+      chrome.tabs.sendMessage(activeTabId, { type: 'ELEMENT_CLICKED' }, function (response) {
+        if (response && response.success) {
+          const pageText = response.text;
+          // Send the page text to the ChatGPT API for summarization
+          // ...
+        }
+      });
+    }
+  });
+}
 export {};
